cfa33fe05852b378ba0b85918a95eecc3b487086
maratkharr <marat.kharr@gmail.com>
2017-10-06 05:21:47 +0300

    Добавлен шаблон для шифра Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,18 +1,18 @@
-L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
-I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+D1 = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
+D2 = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 
 plaintext = input()
 
 # encrypt
 ciphertext = ""
 for c in plaintext.upper():
-    if c.isalpha(): ciphertext += I2L[ (L2I[c] + 3) % 26]
+    if c.isalpha(): ciphertext += D2[ (D1[c] + 3) % 26]
     else: ciphertext += c
 
 # decrypt
 plaintext2 = ""
 for c in ciphertext.upper():
-    if c.isalpha(): plaintext2 += I2L[ (L2I[c] - 3) % 26]
+    if c.isalpha(): plaintext2 += D2[ (D1[c] - 3) % 26]
     else: plaintext2 += c
 
 print(plaintext)
