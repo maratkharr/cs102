00695907470609eda746fd430fdd0fc269bc6528
maratkharr <marat.kharr@gmail.com>
2017-10-06 05:19:44 +0300

    Добавлен шаблон для шифра Виженера

---
+++ homework01/vigener.py
@@ -0,0 +1,24 @@
+plaintext = input()
+key = input()
+ciphertext = ""
+
+# encrypt
+key_length = len(key)
+key_as_int = [ord(i) for i in key]
+plaintext_int = [ord(i) for i in plaintext]
+ciphertext = ''
+for i in range(len(plaintext_int)):
+  value = (plaintext_int[i] + key_as_int[i % key_length]) % 26
+  ciphertext += chr(value + 65)
+
+# decrypt
+key_length = len(key)
+key_as_int = [ord(i) for i in key]
+ciphertext_int = [ord(i) for i in ciphertext]
+plaintext = ''
+for i in range(len(ciphertext_int)):
+  value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
+  plaintext += chr(value + 65)
+    
+print(ciphertext)
+print(plaintext)
